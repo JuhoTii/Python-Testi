e5703c28f4dec0c01f0dcd1dd4e1bc698b0c02ad
Juho Tiihonen <juho.tiihonen@centria.fi>
2025-10-10 11:54:18 +0300

    kikkailuja ja funktioiden kanssa testailua

---
+++ muuttujat.py
@@ -0,0 +1,7 @@
+opiskelija_maara = 1000
+rating = 4.99
+is_published = True
+course_name = "Python testi"
+print(opiskelija_maara)
+print("*" * 10)
+print(1,2,3, sep="--")
---
+++ pelaajafunktio.py
@@ -0,0 +1,10 @@
+p1_hp = 100
+p2_hp = 100
+
+
+def tulosta_pelitilanne():
+    print("Pelaaja 1 HP:" + str(p1_hp) + " Pelaaja 2 HP:" + str(p2_hp))
+
+
+if __name__ == "__main__":
+    tulosta_pelitilanne()
